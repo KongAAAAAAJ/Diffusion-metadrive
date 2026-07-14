ca0d1f6fd0510028aeec16325b96a78c6d4616a7
KongAAAAAAJ <ajkong@tongji.edu.cn>
2026-07-14 17:02:00 +0800

    codex fix

--- scenarios/S6_inject_vehicles.py
+++ scenarios/S6_inject_vehicles.py
@@ -8,12 +8,12 @@ from typing import Callable
 _SAME_LANE_VEHICLES = (
     ("ego_lane_front_1", 30.0, 25.0),
     ("ego_lane_front_2", 55.0, 26.0),
-    # ("ego_lane_front_3", 80.0, 24.0),
-    # ("ego_lane_front_4", 115.0, 28.0),
-    # ("ego_lane_rear_1", -20.0, 26.0),
-    # ("ego_lane_rear_2", -45.0, 23.0),
-    # ("ego_lane_rear_3", -80.0, 24.0),
-    # ("ego_lane_rear_4", -115.0, 21.0),
+    ("ego_lane_front_3", 80.0, 24.0),
+    ("ego_lane_front_4", 115.0, 28.0),
+    ("ego_lane_rear_1", -20.0, 26.0),
+    ("ego_lane_rear_2", -45.0, 23.0),
+    ("ego_lane_rear_3", -80.0, 24.0),
+    ("ego_lane_rear_4", -115.0, 21.0),
 )
 
 _ADJACENT_LANE_OFFSETS = (-40.0, -10.0, 15.0, 35.0)
@@ -28,7 +28,7 @@ def build_s6_traffic_recipes(recipe_factory: Callable[..., object]) -> tuple[obj
             "reference_kind": "block_socket_road",
             "block_id": "g1",
             "socket_index": 1,
-            "spawn_longitude": 30.0,
+            "spawn_longitude": 0.0,
             "target_speed_kmh": 24.0,
             "policy": "idm_merge",
             "merge_front_gap_m": 25.0,
@@ -75,4 +75,5 @@ def build_s6_traffic_recipes(recipe_factory: Callable[..., object]) -> tuple[obj
         },
     )
 
-    return (merge_vehicle, *same_lane_vehicles, adjacent_lane_traffic)
+    # return (merge_vehicle, *same_lane_vehicles, adjacent_lane_traffic)
+    return (merge_vehicle)
